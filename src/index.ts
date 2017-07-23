86f087c430b66ab420dc133e39aa10befd54673c
Pavel Machekhin <pavel.machekhin@gmail.com>
2017-07-23 10:54:21 +0300

    Fix after update

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,10 @@ setPropertyDidChange(() => {
 
 app.registerInitializer({
   initialize(registry) {
-    registry.register(`component-manager:/${app.rootName}/component-managers/main`, ComponentManager)
+    registry.register(`component-manager:/${app.rootName}/component-managers/main`, ComponentManager);
   }
 });
 
-app.renderComponent('todo-app', containerElement, null);
+app.renderComponent('glimmer-todo', containerElement, null);
 
 app.boot();
---
+++ src/ui/components/glimmer-todo/component-test.ts
@@ -0,0 +1,13 @@
+import { setupRenderingTest } from '@glimmer/test-helpers';
+import hbs from '@glimmer/inline-precompile';
+
+const { module, test } = QUnit;
+
+module('Component: glimmer-todo', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await this.render(hbs`<glimmer-todo />`);
+    assert.ok(this.containerElement.querySelector('section.todoapp'));
+  });
+});
