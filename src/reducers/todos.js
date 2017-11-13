e771fad3e6026b71c3d6b2bb285f267a3a2a2916
Pavel Machekhin <pavel.machekhin@gmail.com>
2017-11-13 22:48:10 +0200

    Use native ES6 array functions in favor of lodash methods

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -9,11 +9,6 @@ import {
   LOADING
 } from '../utils/constants/ActionTypes';
 
-import omit from 'lodash-es/omit';
-import omitBy from 'lodash-es/omitBy';
-import mapValues from 'lodash-es/mapValues';
-import defaults from 'lodash-es/defaults';
-import values from 'lodash-es/values';
 import { createSelector } from 'reselect';
 
 const initialState = {
@@ -39,68 +34,62 @@ export default function todos(state, action) {
     }
 
     case COMPLETE_ALL: {
-      let todos = omitBy(state.all, todo => todo.isCompleted === true);
       return {
         ...state,
-        all: todos
+        all: state.all.map(todo => ({ ...todo, isCompleted: true }))
       }
     }
 
     case CLEAR_COMPLETED: {
-      let todos = omitBy(state.all, todo => todo.isCompleted === true);
       return {
         ...state,
-        all: todos
+        all: state.all.map(todo => ({ ...todo, isCompleted: false }))
       }
     }
 
     case DELETE_TODO: {
-      let todos = omitBy(state.all, todo => todo.id === action.id);
-
       return {
         ...state,
-        all: todos
+        all: state.all.filter(todo => todo.id !== action.id)
       }
     }
 
     case EDIT_TODO: {
-      let todos = mapValues(state.all, todo => {
-        return todo.id === action.id ? defaults({
-          text: action.text
-        }, todo) : todo;
-      });
       return {
         ...state,
-        all: {...state.all, ...todos}
+        all: state.all.map(todo => {
+          return todo.id === action.id ? {
+            ...todo,
+            text: action.text
+          } : todo;
+        })
       }
     }
 
     case COMPLETE_TODO: {
-      let todos = mapValues(state.all, todo => {
-        return todo.id === action.id ? defaults({
-          isCompleted: !todo.isCompleted
-        }, todo) : todo;
-      });
-
       return {
         ...state,
-        all: {...state.all, ...todos}
+        all: state.all.map(todo => {
+          return todo.id === action.id ? {
+            ...todo,
+            isCompleted: !todo.isCompleted
+          } : todo;
+        })
       }
     }
 
     case ADD_TODO: {
       const id = Object.values(state.all).reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1;
+
       let todo = {
-        [id]: {
-          id: action.id || id,
-          isCompleted: action.isCompleted || false,
-          text: action.text
-        }
+        id: action.id || id,
+        isCompleted: action.isCompleted || false,
+        text: action.text
       };
 
       return {
         ...state,
-        all: {...state.all, ...todo}
+        all: [...state.all, todo]
       }
     }
 
@@ -117,13 +106,13 @@ export const getTodos = createSelector(
   all,
   filter,
   (all, filter) => {
-    return omitBy(all, todo => {
-      return filter === undefined ? false : filter !== todo.isCompleted;
+    return all.filter(todo => {
+      return filter === undefined ? true : filter !== todo.isCompleted;
     });
   }
 );
 
-export const getAllTodosCount = createSelector(all, (all) => values(all).length);
+export const getAllTodosCount = createSelector(all, (all) => all.length);
 export const getFilter = createSelector(filter, filter => filter);
-export const getTodosCount = createSelector(getTodos, (todos) => values(todos).length);
-export const getCompletedCount = createSelector(all, (all) => values(all).filter(t => t.isCompleted).length);
+export const getTodosCount = createSelector(getTodos, (todos) => todos.length);
+export const getCompletedCount = createSelector(all, (all) => all.filter(t => t.isCompleted).length);
