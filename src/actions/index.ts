75b23da1989b50facec056ecf26b27f20bbbd280
Pavel Machekhin <pavel.machekhin@gmail.com>
2017-12-05 21:57:19 +0200

    Use defined ActionTypes instead of custom string values in actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,19 +1,23 @@
-import { CLEAR_COMPLETED } from "../utils/constants/ActionTypes";
+import {
+  ADD_TODO, CLEAR_COMPLETED, COMPLETE_ALL, COMPLETE_TODO, DELETE_TODO,
+  EDIT_TODO, FETCH_ALL, LOADING
+} from "../utils/constants/ActionTypes";
 import { Todo, TodoAction } from "../utils/types";
+import { SHOW_ACTIVE, SHOW_ALL, SHOW_COMPLETED } from "../utils/constants/TodoFilters";
 
-export const addTodo = (text: string) => dispatch => dispatch(<TodoAction>{ type: 'ADD_TODO', text });
-export const editTodo = (id: number, text: string) => dispatch => dispatch(<TodoAction>{ type: 'EDIT_TODO', id, text });
-export const deleteTodo = (id: number) => dispatch => dispatch(<TodoAction>{ type: 'DELETE_TODO', id });
-export const completeTodo = (id: number) => dispatch => dispatch(<TodoAction>{ type: 'COMPLETE_TODO', id });
-export const completeAll = () => dispatch => dispatch(<TodoAction>{ type: 'COMPLETE_ALL' });
-export const clearCompleted = () => dispatch => dispatch(<TodoAction>{ type: 'CLEAR_COMPLETED' });
+export const addTodo = (text: string) => dispatch => dispatch(<TodoAction>{ type: ADD_TODO, text });
+export const editTodo = (id: number, text: string) => dispatch => dispatch(<TodoAction>{ type: EDIT_TODO, id, text });
+export const deleteTodo = (id: number) => dispatch => dispatch(<TodoAction>{ type: DELETE_TODO, id });
+export const completeTodo = (id: number) => dispatch => dispatch(<TodoAction>{ type: COMPLETE_TODO, id });
+export const completeAll = () => dispatch => dispatch(<TodoAction>{ type: COMPLETE_ALL });
+export const clearCompleted = () => dispatch => dispatch(<TodoAction>{ type: CLEAR_COMPLETED });
 
-export const showAll = () => dispatch => dispatch(<TodoAction>{ type: 'SHOW_ALL' });
-export const showActive = () => dispatch => dispatch(<TodoAction>{ type: 'SHOW_ACTIVE' });
-export const showCompleted = () => dispatch => dispatch(<TodoAction>{ type: 'SHOW_COMPLETED' });
+export const showAll = () => dispatch => dispatch(<TodoAction>{ type: SHOW_ALL });
+export const showActive = () => dispatch => dispatch(<TodoAction>{ type: SHOW_ACTIVE });
+export const showCompleted = () => dispatch => dispatch(<TodoAction>{ type: SHOW_COMPLETED });
 
 export const fetch = () => async (dispatch) => {
-  dispatch({ type: 'LOADING' });
+  dispatch(<TodoAction>{ type: LOADING });
 
   const todos = await new Promise<Todo[]>(resolve => {
     setTimeout(() => {
@@ -27,7 +31,7 @@ export const fetch = () => async (dispatch) => {
 
   todos.map(todo => dispatch(<TodoAction>{ type: 'ADD_TODO', ...todo }));
 
-  return dispatch(<TodoAction>{ type: 'FETCH_ALL' });
+  return dispatch(<TodoAction>{ type: FETCH_ALL });
 };
 
 export default {};
